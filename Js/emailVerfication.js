67c4f1664be2ea81570cf3fdb5e69d2c36946304
Aisha-Naeem <ayshaa.naem@gmail.com>
2024-04-14 20:26:40 +0500

    emailVerificationPage -> OTP generation

---
+++ Js/emailVerfication.js
@@ -0,0 +1,73 @@
+const input = document.querySelectorAll("input"),
+button = document.querySelectorAll("button"),
+email = document.getElementById("email"),
+expire = document.getElementById("expire");
+
+let OTP ="";
+function generateOTPs()
+{
+    console.log(
+        Math.floor(Math.random() * 10)  + " " +
+        Math.floor(Math.random() * 10)   + " " +
+        Math.floor(Math.random() * 10)   + " " +
+        Math.floor(Math.random() * 10)
+    );
+
+    input[0].focus();
+    expire.innerText= 10;
+    const expireInterval = setInterval(()=>
+    {
+        expire.innerText--;
+        if (expire.innerText == 0)
+        {
+            clearInterval(expireInterval);
+        }
+
+    },1000);
+    alert("Your OTP is: " + OTP);
+}
+
+function clearOTPs()
+{
+    input.forEach((input, index)=>{
+        input.addEventListener("keyup",(e)=>{
+            const currentInput = input,
+            nextInput = input.nextElementSibling,
+            prevInput = input.previousElementSibling;
+
+            if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value != "")
+            {
+                nextInput.removeAttribute("disabled",true);
+                nextInput.focus();
+            }
+
+            if(e.key === "Backspace")
+            {
+                inputs.forEach((input, index1)=>{
+                    if(index <= index1 && prevInput)
+                   {
+                    input.setAttribute("disabled",true);
+                    prevInput.focus();
+                    prevInput.value="";
+                   }
+                });
+            }
+
+            if(!inputs[3].disabled && inputs[3].value !== "")
+            {
+                inputs[3].blur();
+                button.classList.add("active");
+                return;
+            }
+        });
+    });
+
+    window.addEventListener("load",()=>{
+        let x = "chayeshanaeem856@gmail.com";
+        if(x){
+            email.innerText=x;
+            generateOTPs();
+        }
+    });
+      
+}
